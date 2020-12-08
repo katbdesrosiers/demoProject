324fbd1b88e894a41169c17a3fbe681e2ca9710d
Katherine Desrosiers <katbdesrosiers@gmail.com>
2020-12-08 13:16:21 -0600

    add style to code page and update javscript

--- src/js/main.js
+++ src/js/main.js
@@ -72,6 +72,8 @@ function checkCode() {
   const codeInput = document.getElementById("code").value;
   const background = document.querySelector("body");
   const h1 = document.querySelector("h1");
+  const input = document.querySelector("input");
+  const h2 = document.querySelector("h2");
 
   if (codeInput === "1107") {
     alert("Correct code entered.");
@@ -79,7 +81,10 @@ function checkCode() {
     background.style.backgroundSize = "cover";
     h1.innerHTML = "Code Accepted";
     h1.style.fontSize = "120px";
-    h1.style.color = "black";
+    h1.style.color = "rgb(175, 28, 28)";
+    input.style.display = "none";
+    button.style.display = "none";
+    h2.style.display = "none";
   } else {
     alert("Error. Incorrect code entered.");
   }
